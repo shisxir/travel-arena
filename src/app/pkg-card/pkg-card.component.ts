9143fc7a9cf88eb88eb55df27767d7b5ede7abe1
Shishir Shrivastava <shrivastavapvt01@gmail.com>
2024-04-15 04:53:26 +0530

    branch change required

---
+++ src/app/pkg-card/pkg-card.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pkg-card',
+  templateUrl: './pkg-card.component.html',
+  styleUrl: './pkg-card.component.css'
+})
+export class PkgCardComponent {
+
+}
---
+++ src/app/pkg-carousel/pkg-carousel.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pkg-carousel',
+  templateUrl: './pkg-carousel.component.html',
+  styleUrl: './pkg-carousel.component.css'
+})
+export class PkgCarouselComponent {
+
+}
