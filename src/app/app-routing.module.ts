38f8dea31c93ce30569b394a85a57ded1259c24b
Shishir Shrivastava <shishir.shrivastava@relanto.ai>
2024-04-18 01:46:21 +0530

    post vyshakh css changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -21,7 +21,7 @@ const routes: Routes = [
   { path: 'src\app\footer', component: FooterComponent},
   { path: 'booking', component:BookingComponent},
   // { path: 'login', component: LoginComponent },
-  {path:'',component:HomepageComponent}
+  // {path:'',component:HomepageComponent}
 
 ];
 
