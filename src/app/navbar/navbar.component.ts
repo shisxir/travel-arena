d81fd0b711696df046704a3e73074749b3c6e1d6
Shishir Shrivastava <shishir.shrivastava@relanto.ai>
2024-04-17 11:25:17 +0530

    navbar changes

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
-import { Component, HostListener, OnInit} from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -6,11 +7,12 @@ import { Component, HostListener, OnInit} from '@angular/core';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
-  isNavbarCollapsed:boolean = true;
+  activeLink: string = '';
+  isNavbarCollapsed: boolean = true;
   ngOnInit(): void {
     this.isNavbarCollapsed = false;;
   }
-
+  constructor(private router: Router) { }
   @HostListener('document:click', ['$event'])
   clickout(event: Event) {
     if (!(event.target as HTMLElement).closest('.navbar-collapse')) {
@@ -19,14 +21,29 @@ export class NavbarComponent implements OnInit {
   }
 
   toggleNavbar() {
-    if(this.isNavbarCollapsed){
-      this.isNavbarCollapsed=false;
-    }else{
-      this.isNavbarCollapsed=true;
+    if (this.isNavbarCollapsed) {
+      this.isNavbarCollapsed = false;
+    } else {
+      this.isNavbarCollapsed = true;
     }
   }
 
   closeNavbar() {
     this.isNavbarCollapsed = true;
+  }
+
+  isPackagesPage(): boolean {
+    return this.router.url === '/packages';
+  }
+  isBookPage(): boolean {
+    ;
+    return this.router.url === '/book';
+  }
+
+  isHomePage(): boolean {
+    return this.router.url === '/home';
+  }
+  isAboutPage(): boolean {
+    return this.router.url === '/about';
   }
 }
