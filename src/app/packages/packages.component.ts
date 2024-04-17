df8d4b91d0cec4ac5a0b265ba68003bc36ade785
Shishir Shrivastava <shishir.shrivastava@relanto.ai>
2024-04-17 12:50:37 +0530

    booking final

--- src/app/packages/packages.component.ts
+++ src/app/packages/packages.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-packages',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrl: './packages.component.scss'
 })
 export class PackagesComponent {
-
+  constructor(private router: Router){}
 }
