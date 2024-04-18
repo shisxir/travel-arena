6c4599caed5794722f3593e69d04a3bddd7181a6
Pranav Srivastava <pranav.srivastava@relanto.ai>
2024-04-18 11:30:44 +0530

    pranav

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,7 +1,6 @@
-import { Component } from '@angular/core';
-import { NgForm } from '@angular/forms';
-import { Router } from '@angular/router';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Component} from '@angular/core';
+import { NavigationExtras, Router } from '@angular/router';
+import { FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 
 @Component({
@@ -10,30 +9,57 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   styleUrl: './search.component.scss'
 })
 export class SearchComponent {
-  searchForm!: FormGroup;
+
+  searchForm:FormGroup =this.formBuilder.group({
+    destination : ['', Validators.required],
+    departure : ['',Validators.required],
+    duration : ['', Validators.required],
+    guests : ['',[Validators.required,Validators.min(1)]]
+  });;
   destinations: string[] = ['Ladakh', 'Lakshadweep', 'Amritsar', 'Kedarnath','Agra', 'Kerala'];
   durations: number[] = [1,2,3,4,5,6];
-  duration?: number=NaN;
-  selectedDestination!: string;
-  todayDate: Date = new Date();
+  todayDate: string = new Date().toISOString().split('T')[0];
+  // invalidForm:boolean=false;
 
-  constructor(private router:Router, private formBuilder: FormBuilder){
-    this.searchForm = this.formBuilder.group({
-      destination: ['', Validators.required],
-      duration: ['', Validators.required],
-      departureDate: ['',[Validators.required,this.dateValidator]],
-      guests: ['',[Validators.required,Validators.min(1)]]
-    });
+
+  constructor(private router:Router, private formBuilder: FormBuilder){}
+
+  isFormInvalid(){
+    return this.searchForm.invalid;
   }
-  
-  dateValidator(control:any){
-    const selectedDate = new Date(control.value);
-    const currentDate = new Date();
-    return selectedDate > currentDate ? null : { invalidDate: true };
+
+  onFocus(event:any){
+    event.target.type = 'date';
+  }
+  onBlur(event:any){
+    event.target.type = 'text';
   }
 
   onSubmit(){
-    console.log(this.selectedDestination)
-    this.router.navigate(['/booking',this.selectedDestination])
+    
+    // if(this.searchForm.invalid){
+    //   this.invalidForm=true;
+    //   return;
+    // }
+    // this.invalidForm=false;
+
+
+    const queryParams: any = { package: this.searchForm.get('destination')?.value };
+    const navigationExtras: NavigationExtras = {
+      queryParams,
+    };
+
+    // sending data to local storage for booking page
+    const destination = this.searchForm.get('destination')?.value;
+    const departure = this.searchForm.get('departure')?.value;
+    const duration = this.searchForm.get('duration')?.value;
+    const guests = this.searchForm.get('guests')?.value;
+    localStorage.setItem('destination', destination);
+    localStorage.setItem('departure', departure);
+    localStorage.setItem('duration', duration);
+    localStorage.setItem('guests', guests);
+    // data stored in local storage
+
+    this.router.navigate(['/booking'],navigationExtras)
   }
 }
